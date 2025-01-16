ca8ffa3ed92a0af6427562a550d4c364537b926a
Malik Talha Tariq <maliktalha123455@gmail.com>
2025-01-16 23:30:45 +0500

    Style Changed

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,30 +1,35 @@
 import Link from 'next/link'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import React from 'react'
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header className='bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg'>
+    <header className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow-2xl'>
     <div className='max-w-6xl mx-auto flex justify-between items-center p-3'>
-      {/* logo */}
-      <Link href='/' className='text-2xl font-extrabold group cursor-pointer'>
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600'>
-          Orator
-        </span>
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600'>
-          Path
-        </span>
-      </Link>
+    {/* <img src="/OP-New.png" alt="Orator Path Logo" style={{ width: '50px', height: '50px', borderRadius: '45%' }} /> */}
+    {/* logo */}
+    <Link href='/' className='text-2xl font-medium group cursor-pointer'>
+  <span className='text-white group-hover:text-gray-300 drop-shadow-md transition-all duration-300'>
+    Orator
+  </span>
+  <span className='text-white group-hover:text-gray-300 drop-shadow-md transition-all duration-300'>
+    Path
+  </span>
+</Link>
+
+
+
       {/* add a navigation menu */}
       <nav>
         <ul className='flex gap-4'>
-          <Link href='/'>Home</Link>
-          <Link href='/about'>About</Link>
+          <Link href='/' className='text-white group-hover:text-gray-300 drop-shadow-md transition-all duration-300'>Home</Link>
+          <Link href='/about' className='text-white group-hover:text-gray-300 drop-shadow-md transition-all duration-300'>About</Link>
           <SignedIn>
             <UserButton/>
           </SignedIn>
           <SignedOut>
-            <SignInButton /> 
+            <SignInButton className='text-white group-hover:text-gray-300 drop-shadow-md transition-all duration-300'/> 
           </SignedOut>
         </ul>
       </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='text-red-500'>Home</div>
+    <div className='flex items-center justify-center h-screen bg-gray-900'>
+  <h1 className='text-6xl font-bold text-white tracking-wide drop-shadow-md'>
+    Welcome to <span className='text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'>Home Page</span>
+  </h1>
+</div>
+
+
   )
 }
