30c74a5722786ce305a03dc420b3214d2d0b99b1
Malik Talha Tariq <maliktalha123455@gmail.com>
2025-03-19 20:51:04 +0500

    Created a new page for audio conversion

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className='flex items-center justify-center h-screen bg-gray-900'>
-  <h1 className='text-6xl font-bold text-white tracking-wide drop-shadow-md'>
-    Welcome to <span className='text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'>Home Page</span>
-  </h1>
+<h1 className='text-4xl font-bold text-white tracking-wide drop-shadow-md'>
+<Link href='/audioanalaysis' className='text-white group-hover:text-gray-300 drop-shadow-md transition-all duration-300'>
+<span>
+Get Presentation Feedback
+</span>
+</Link>
+</h1>
 </div>
-
-
   )
 }
