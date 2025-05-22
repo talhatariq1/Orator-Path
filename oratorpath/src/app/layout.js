0458018d0f1cc50370fb18c7baa3e9a6a9b61106
shamas92 <127528949+talhatariq1@users.noreply.github.com>
2025-05-23 01:10:03 +0500

    Remove DirectHydrationFix and related hydration components

--- oratorpath/src/app/layout.js
+++ oratorpath/src/app/layout.js
@@ -11,15 +11,7 @@ import { dark } from '@clerk/themes';
 import Loader from "./components/Loader";
 import NavigationBar from "./components/NavigationBar";
 import FooterWrapper from "./components/FooterWrapper";
-import dynamic from "next/dynamic";
 import { RedirectProvider } from "../lib/context/RedirectContext";
-
-// Use the client component wrapper
-const HydrationFix = dynamic(() => import('./components/client/HydrationFix'));
-const SVDAttributeRemover = dynamic(() => import('./components/client/SVDAttributeRemover'));
-const AttributeDebugger = dynamic(() => import('./components/client/AttributeDebugger'));
-const HydrationErrorFix = dynamic(() => import('./components/client/HydrationErrorFix'));
-const ScriptMonitor = dynamic(() => import('./components/client/ScriptMonitor'));
 
 // Fonts are now imported from lib/fonts.js
 
@@ -32,33 +24,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={getFontVariables()}>
       <body className="min-h-screen bg-black text-white">
-        <HydrationErrorFix />
-        <ScriptMonitor />
-        <AttributeDebugger />
-        <SVDAttributeRemover />
-        <DirectHydrationFix>
-          <ClerkProvider appearance={{ baseTheme: dark }}>
-            <RedirectProvider>
-              {/* The dashboard layout will handle its own structure */}
-              {/* This layout only applies to non-dashboard routes */}
-              <div className="relative min-h-screen flex flex-col">
-                <div className="fixed inset-0 -z-10 pattern-bg"></div>
-                <NavigationBar />
-                <ClerkLoading>
-                  <Loader />
-                </ClerkLoading>
-                <ClerkLoaded>
-                  <main className="relative z-10 flex-grow">
-                    {children}
-                  </main>
-                  <div className="z-10">
-                    <FooterWrapper />
-                  </div>
-                </ClerkLoaded>
-              </div>
-            </RedirectProvider>
-          </ClerkProvider>
-        </DirectHydrationFix>
+        <ClerkProvider appearance={{ baseTheme: dark }}>
+          <RedirectProvider>
+            {/* The dashboard layout will handle its own structure */}
+            {/* This layout only applies to non-dashboard routes */}
+            <div className="relative min-h-screen flex flex-col">
+              <div className="fixed inset-0 -z-10 pattern-bg"></div>
+              <NavigationBar />
+              <ClerkLoading>
+                <Loader />
+              </ClerkLoading>
+              <ClerkLoaded>
+                <main className="relative z-10 flex-grow">
+                  {children}
+                </main>
+                <div className="z-10">
+                  <FooterWrapper />
+                </div>
+              </ClerkLoaded>
+            </div>
+          </RedirectProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
