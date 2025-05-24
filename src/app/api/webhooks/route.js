3c128959c54d0a1057a1a3cf3f5b9c56352d9c8b
shamas92 <127528949+talhatariq1@users.noreply.github.com>
2025-05-24 21:23:21 +0500

    updated route file

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,7 +1,5 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
-
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
