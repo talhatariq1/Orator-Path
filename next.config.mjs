62b9e593a8d977533cb3d778acc5c4994d6bc448
shamas92 <127528949+talhatariq1@users.noreply.github.com>
2025-05-24 21:41:09 +0500

    changes on root js files

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
